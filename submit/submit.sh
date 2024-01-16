8d3e45f222b1caab19472f6008bdf47c7be91757
Guido Schmutz <guido.schmutz@trivadis.com>
2024-01-16 21:40:53 +0100

    Update submit.sh

--- submit/submit.sh
+++ submit/submit.sh
@@ -15,7 +15,7 @@ else
     if [ ! -z "${SPARK_APPLICATION_PYTHON_LOCATION}" ]; then
         echo "Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URL}"
         echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
-        PYSPARK_PYTHON=python3  /spark/bin/spark-submit \
+        PYSPARK_PYTHON=python3  ${SPARK_HOME}/bin/spark-submit \
             --master ${SPARK_MASTER_URL} \
             ${SPARK_SUBMIT_ARGS} \
             ${SPARK_APPLICATION_PYTHON_LOCATION} ${SPARK_APPLICATION_ARGS}
